a74d134069e0d2c7bbdcecac729e92241797bd17
jaredliw <jaredliw@gmail.com>
2021-10-16 20:59:30 +0800

    Pandas data analysis

---
+++ analytics.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from json import load
+
+import pandas as pd
+
+with open("juejin_articles.json", "r", encoding="utf-8") as file:
+    json_data = load(file)
+
+data = []
+for key in json_data.keys():
+    data += json_data[key]
+del json_data
+
+df = pd.DataFrame(data)
+del data
+
+df["mtime"] = pd.to_datetime(df['mtime'], unit='s')
+df["time_diff"] = (datetime.now() - df["mtime"]).dt.days
+df["score"] = (df["view_count"] * 0.1 +
+               df["comment_count"] * 0.2 +
+               df["digg_count"] * 0.3 +
+               df["collect_count"] * 0.4) / (df["time_diff"] + 2) ** 1.5

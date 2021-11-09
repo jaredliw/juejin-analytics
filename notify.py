f2a0de96d867939939291a96cf135dfbf708b0d0
jaredliw <jaredliw@gmail.com>
2021-11-09 17:33:41 +0800

    add docstrings, format lines

--- notify.py
+++ notify.py
@@ -2,7 +2,6 @@ from os import environ
 
 from __init__ import session, escape_markdown
 
-
 api_send = "https://api.telegram.org/bot{}/sendMessage"
 try:
     telegram_bot_token = environ["TELEGRAM_BOT_TOKEN"]
@@ -11,13 +10,15 @@ except KeyError:
     raise LookupError("TELEGRAM_BOT_TOKEN and/or TELEGRAM_CHAT_ID is not set in the environment variable")
 
 class TelegramError(Exception):
+    """TelegramError is raised when the response from Telegram API is not 200 OK."""
     pass
 
 
 def send_message(message):
+    """Send a message via Telegram. Message is written in markdown format."""
     data = {
         "chat_id": telegram_chat_id,
-        "parse_mode": "MarkdownV2",
+        "parse_mode": "MarkdownV2",  # do not use markdown (version 1)!
         "text": message
     }
     response = session.get(api_send.format(telegram_bot_token), params=data).json()
@@ -31,11 +32,11 @@ if __name__ == "__main__":
 
     from translate import get_pending_articles
 
-
     data = get_pending_articles()
 
     sent_items = []
     try:
+        # A custom file is record the items that have been sent to prevent repeating messages
         with open("sent.txt", "r") as file:
             sent_items = file.read().split("\n")
     except FileNotFoundError:
@@ -43,9 +44,10 @@ if __name__ == "__main__":
 
     ids = []
     for item in data:
-        print(sent_items, item)
         if str(item["id"]) not in sent_items:
-            send_message(f"{escape_markdown(item['recommend_name'])}推荐了一篇文章：[{escape_markdown(item['title_src'])}]({item['link_url']})（{item['word_count']} 单词），快去[审核](https://juejin.cn/translate/manage)吧！")
+            send_message(f"{escape_markdown(item['recommend_name'])}推荐了一篇文章："
+                         f"[{escape_markdown(item['title_src'])}]({item['link_url']})（{item['word_count']} 单词），"
+                         f"快去[审核](https://juejin.cn/translate/manage)吧！")
         ids.append(item["id"])
         sleep(1)
 

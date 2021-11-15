cb70c63895624e96ac2301ac4bcbff8a949ac052
wolfishLamb <57293776+jaredliw@users.noreply.github.com>
2021-11-16 00:21:49 +0800

    Create user.py

---
+++ user.py
@@ -0,0 +1,22 @@
+"""Juejin login and user profile."""
+from __init__ import session, login_required, response_post_check, api_profile_id, cookie, api_user
+
+
+@login_required
+@response_post_check
+def get_profile_id():
+    """Get the profile ID of current logged in user."""
+    return session.get(api_profile_id, headers={"cookie": f"sessionid={cookie};"}).json()
+
+@login_required
+@response_post_check
+def get_profile(profile_id=None):
+    """Get user profile, default to the current logged in user."""
+    if profile_id is None:
+        profile_id = get_profile_id()["profile_id"]
+
+    return session.get(api_user, headers={"cookie": f"sessionid={cookie};"}).json()
+
+
+if __name__ == "__main__":
+    print(get_profile())
